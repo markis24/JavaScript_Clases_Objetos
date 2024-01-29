d2216d2c18428e8ba04db21869d0e7c713e24ee9
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-01-29 18:07:24 +0100

    Archivos Modul & Cicle

---
+++ js/Cicle.js
@@ -0,0 +1,21 @@
+class cicle {
+    constructor(num, nom, numEdicions,data,moduls) {
+        this.num = num;
+        this.nom = nom;
+        // Valor inicial de numEdicions = 0
+        this.numEdicions = 0;
+        this.data = data;
+        this.moduls = [];
+    }
+}
+
+toString(); {
+    return `MP${this.num}. ${this.nom} ${this.moduls} ${this.data}`;
+}
+
+/*
+
+setNumEdicions (){
+    
+}
+*/
---
+++ js/Modul.js
@@ -0,0 +1,10 @@
+class Modul {
+    constructor(num, nom, hores) {
+        this.num = num;
+        this.nom = nom;
+        this.hores = hores;
+    }
+}
+toString(); {
+    return `MP${this.num}. ${this.nom} (${this.hores}h ${this.data})`;
+}
